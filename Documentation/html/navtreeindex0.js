d71b2ea1c474e94891249b4f0a19e2a91eb7938e
Smoph <Smoph@users.noreply.github.com>
2018-05-24 15:41:20 +0200

    Initial commit

---
+++ Documentation/html/navtreeindex0.js
@@ -0,0 +1,11 @@
+var NAVTREEINDEX0 =
+{
+"dir_68267d1309a1af8e8297ef4c3efbcdba.html":[0,0,0],
+"files.html":[0,0],
+"globals.html":[0,1,0],
+"globals_func.html":[0,1,1],
+"index.html":[],
+"main_8cpp.html":[0,0,0,0],
+"main_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4":[0,0,0,0,0],
+"pages.html":[]
+};
---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 24.05.2018
+ *      Author: Smoph, Mollni
+ */
+
+#include <iostream>
+
+int main ()
+{
+
+	std::cout << "This is just the Beginning!"<< std::endl;
+
+	return (EXIT_SUCCESS);
+}
+
+
+
